9fecc6d13600f1c4b182310e4cc139837ed02cb6
Fabricio <pozzonifabricio@gmail.com>
2020-11-19 08:01:07 -0300

    funcion-login

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import uuid
 from werkzeug.security import generate_password_hash,check_password_hash
 import jwt
 import datetime
+from functools import wraps
 
 app=Flask(__name__)
 
@@ -87,6 +88,23 @@ def delete_user(public_id):
 	db.session.commit()
 
 	return jsonify({'message':'Usuario eliminado'})
+
+@app.route('/login',methods=['GET'])
+def login():
+	auth=request.authorization
+
+	if not auth or not auth.username or not auth.password:
+		return make_response('Could not verify',401,{'WWW-Authenticate':'Basic realm="Login required'})
+
+	user=User.query.filter_by(name=auth.username).first()
+
+	if not user:
+		return make_response('No se encontro el usuario')
+
+	if check_password_hash(user.password,auth.password):
+		token=jwt.encode({'public_id':user.public_id,'exp': datetime.datetime.utcnow()+ datetime.timedelta(minutes=60)},app.config['SECRET_KEY'])
+		return jsonify({'token':token.decode('UTF-8')})
+	return make_response('Could not verify',401,{'WWW-Authenticate':'Basic realm="Login required"'})
 
 if __name__ == '__main__':
 	app.run(debug=True)
