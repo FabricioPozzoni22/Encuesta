2f655bf091f9fd5cb78cc0c02a1e7087be792da6
Fabricio <pozzonifabricio@gmail.com>
2020-11-19 07:21:36 -0300

    Set db

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
 app.config['SECRET_KEY']='secret'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:''@localhost/encuesta'
 
+db=SQLAlchemy(app)
 
 if __name__ == '__main__':
 	app.run(debug=True)
