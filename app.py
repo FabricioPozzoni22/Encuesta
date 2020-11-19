edc3dd10636b61d356dc34066fac5c4f78b0af4e
Fabricio <pozzonifabricio@gmail.com>
2020-11-19 07:43:12 -0300

    funcion-create_user

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask,request,jsonify,make_response
 from flask_sqlalchemy import SQLAlchemy
+import uuid 
+from werkzeug.security import generate_password_hash,check_password_hash
+import jwt
+import datetime
 
 app=Flask(__name__)
 
@@ -15,6 +19,17 @@ class User(db.Model):
 	name=db.Column(db.String(50))
 	password=db.Column(db.String(80))
 	admin=db.Column(db.Boolean)
+
+
+@app.route('/user',methods=['POST'])
+def create_user():
+	data=request.get_json()
+	hashed_password=generate_password_hash(data['password'],method='sha256')
+	new_user=User(public_id=str(uuid.uuid4()),name=data['name'],password=hashed_password,admin=False)
+	db.session.add(new_user)
+	db.session.commit()
+
+	return jsonify({'message':'Nuevo usuario creado'})
 
 if __name__ == '__main__':
 	app.run(debug=True)
